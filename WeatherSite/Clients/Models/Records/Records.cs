4d3ca325138f47645e80c394ddff27e6e7ab7899
mirusser <mirusser@gmail.com>
2021-03-21 18:38:54 +0100

    # moved records

--- WeatherSite/Clients/Models/Records/Records.cs
+++ WeatherSite/Clients/Models/Records/Records.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace WeatherSite.Clients.Models.Records
 {
-    public class Records
-    {
-    }
+    public record Coord(decimal Lon, decimal Lat);
+    public record City(decimal Id, string Name, string State, string Country, Coord Coord);
+    public record WeatherForecast(DateTime Date, int TemperatureC, int TemperatureF, string Summary);
 }
