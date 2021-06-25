15103ae9d38ca5e92456c8ad727c33d701bcef3f
mirusser <mirusser@gmail.com>
2021-06-25 22:16:35 +0200

    Added generic loader using jquery toast (+ some UI improvments)

---
+++ src/WeatherSite/Controllers/SharedController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherSite.Controllers
+{
+    public class SharedController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> BootstrapLoader()
+        {
+            return PartialView("_BootstrapLoader");
+        }
+    }
+}
---
+++ src/WeatherSite/wwwroot/js/toast-loader.js
@@ -0,0 +1,30 @@
+﻿
+
+function showBootstrapLoader(title) {
+
+    if (title == null || title.trim() == '') {
+
+        title = 'Getting data';
+    }
+
+    $.post(
+        '/Shared/BootstrapLoader',
+        {},
+        function (response) {
+
+            $(document).Toasts('create', {
+                class: 'bg-info loading-toast',
+                icon: 'fas fa-download fa-lg',
+                title: title,
+                close: false,
+                body: response,
+            });
+        }
+    );
+}
+
+function removeBoostrapLoader() {
+    setTimeout(function () {
+        $('.loading-toast').remove();
+    }, 500);
+}
