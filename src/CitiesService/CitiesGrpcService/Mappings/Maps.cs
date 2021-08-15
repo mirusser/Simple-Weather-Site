b95b80e69d362cc1ff8e335048d882fa5711851f
mirusser <mirusser@gmail.com>
2021-08-15 22:01:45 +0200

    CitiesService: added CitiesGrpcService (needs a bit of refactoring)

---
+++ src/CitiesService/CitiesGrpcService/Mappings/Maps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dto;
+using AutoMapper;
+
+namespace CitiesGrpcService.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<CityDto, City>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => (double)src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.State, opt => opt.MapFrom(src => src.State))
+                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+                .ForMember(dest => dest.Coord.Lon, opt => opt.MapFrom(src => (double)src.Coord.Lon))
+                .ForMember(dest => dest.Coord.Lat, opt => opt.MapFrom(src => (double)src.Coord.Lat));
+        }
+    }
+}
---
+++ src/CitiesService/CitiesGrpcService/Services/CitiesService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using CitiesGrpcService;
+using Application.Interfaces.Managers;
+using AutoMapper;
+
+namespace CitiesGrpcService
+{
+    public class CitiesService : Cities.CitiesBase
+    {
+        private readonly ICityManager _cityManager;
+        private readonly IMapper _mapper;
+
+        public CitiesService(
+            ICityManager cityManager,
+            IMapper mapper)
+        {
+            _cityManager = cityManager;
+            _mapper = mapper;
+        }
+
+        public override Task<CitiesPaginationInfo> GetCitiesPaginationInfo(CitiesPaginationInfoRequest request, ServerCallContext context)
+        {
+            return base.GetCitiesPaginationInfo(request, context);
+        }
+
+        public override Task<CitiesPaginationReply> GetCitiesPagination(CitiesPaginationRequest request, ServerCallContext context)
+        {
+            return base.GetCitiesPagination(request, context);
+        }
+
+        //TODO: add automapper
+        public override async Task GetCitiesStream(
+            CitiesStreamRequest request,
+            IServerStreamWriter<City> responseStream,
+            ServerCallContext context)
+        {
+            try
+            {
+                var citiesPaginationDto = await _cityManager.GetCitiesPagination(request.NumberOfCities, request.PageNumber);
+
+                List<City> cities = 
+                    citiesPaginationDto
+                    .Cities
+                    .Select(
+                        x => new City() 
+                        {
+                            Id = (double)x.Id,
+                            Name = x.Name,
+                            State = x.State,
+                            Country = x.Country,
+                            Coord = new Coord()
+                            {
+                                Lat = (double)x.Coord.Lat,
+                                Lon = (double)x.Coord.Lon
+                            }
+                        })
+                    .ToList();
+                //var cities = _mapper.Map<List<City>>(citiesPaginationDto.Cities);
+
+                foreach (var city in cities)
+                {
+                    await responseStream.WriteAsync(city);
+                }
+            }
+            catch (Exception ex)
+            {
+                var x = ex;
+            }
+        }
+    }
+}
