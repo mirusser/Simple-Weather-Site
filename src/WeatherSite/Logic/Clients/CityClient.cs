d58a5ff18c915fea06abe1bec74e75c452efebe5
mirusser <mirusser@gmail.com>
2021-08-15 16:33:36 +0200

    WeatherSite: UriHelper.CreateAbsoluteUrl

--- src/WeatherSite/Logic/Clients/CityClient.cs
+++ src/WeatherSite/Logic/Clients/CityClient.cs
@@ -9,6 +9,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Web;
 using WeatherSite.Clients.Models.Records;
+using WeatherSite.Logic.Helpers;
 using WeatherSite.Settings;
 
 namespace WeatherSite.Clients
@@ -18,32 +19,30 @@ namespace WeatherSite.Clients
         #region Properties
         private readonly HttpClient _httpClient;
         private readonly ApiEndpoints _apiEndpoints;
-        private readonly ILogger<CityClient> _logger;
-        public string Url { get; set; }
         #endregion
 
         public CityClient(
             HttpClient httpClient,
-            IOptions<ApiEndpoints> options,
-            ILogger<CityClient> logger)
+            IOptions<ApiEndpoints> options)
         {
             _httpClient = httpClient;
             _apiEndpoints = options.Value;
-            _logger = logger;
         }
 
         public async Task<List<City>> GetCitiesByName(string cityName, int limit = 10)
         {
             string url = $"{_apiEndpoints.CitiesServiceApiUrl}{HttpUtility.UrlEncode(cityName)}/{HttpUtility.UrlEncode(limit.ToString())}";
-            List<City> cities = await _httpClient.GetFromJsonAsync<List<City>>(url);
+            List<City> cities =
+                await _httpClient.GetFromJsonAsync<List<City>>(url);
 
             return cities;
         }
 
-        public async Task<CitiesPagination> GetCitiesPagination(int pageNumber = 1, int numberOfCities = 25)
+        public async Task<CitiesPagination?> GetCitiesPagination(int pageNumber = 1, int numberOfCities = 25)
         {
             string url = $"{_apiEndpoints.CitiesServiceApiUrl}GetCitiesPagination/{HttpUtility.UrlEncode(numberOfCities.ToString())}/{HttpUtility.UrlEncode(pageNumber.ToString())}";
-            var citiesPagination = await _httpClient.GetFromJsonAsync<CitiesPagination>(url);
+
+            CitiesPagination? citiesPagination = await _httpClient.GetFromJsonAsync<CitiesPagination>(url);
 
             return citiesPagination;
         }
---
+++ src/WeatherSite/Logic/Helpers/UriHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WeatherSite.Logic.Helpers
+{
+    public static class UriHelper
+    {
+        public static string CreateAbsoluteUrl(string? url, Dictionary<string, string>? parameters, string? fragment = null)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            var uriBuilder = new UriBuilder(url);
+            var uriParameters = HttpUtility.ParseQueryString(string.Empty);
+
+            if (parameters is not null && parameters.Any())
+            {
+                foreach (var parameter in parameters)
+                {
+                    uriParameters[parameter.Key] = HttpUtility.UrlEncode(parameter.Value);
+                }
+            }
+
+            uriBuilder.Query = uriParameters.ToString();
+
+            if (!string.IsNullOrEmpty(fragment))
+            {
+                uriBuilder.Fragment = fragment;
+            }
+
+            Uri uri = uriBuilder.Uri;
+
+            if (!Uri.TryCreate(uri.ToString(), UriKind.Absolute, out uri))
+            {
+                throw new ArgumentException(nameof(url));
+            }
+
+            return uri.OriginalString;
+        }
+    }
+}
