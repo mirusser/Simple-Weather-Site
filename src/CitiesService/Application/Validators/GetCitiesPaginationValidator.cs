a6eaadbb846f3271c4261b9e01e85c8a441ea564
mirusser <mirusser@gmail.com>
2021-10-15 23:04:28 +0200

    CitiesService: Added GetCitiesPaginationValidator

---
+++ src/CitiesService/Application/Validators/GetCitiesPaginationValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Queries;
+using FluentValidation;
+
+namespace Application.Validators
+{
+    public class GetCitiesPaginationValidator : AbstractValidator<GetCitiesPaginationQuery>
+    {
+        public GetCitiesPaginationValidator()
+        {
+            RuleFor(g => g.NumberOfCities)
+                .Cascade(CascadeMode.Continue)
+                .GreaterThanOrEqualTo(1).WithMessage("{PropertyName} should be bigger than 0.")
+                .LessThanOrEqualTo(100).WithMessage("{PropertyName} can't be bigger than 100 due to bandwidth limitation");
+
+            RuleFor(g => g.PageNumber)
+                .Cascade(CascadeMode.Continue)
+                .GreaterThanOrEqualTo(1).WithMessage("{PropertyName} should be bigger than 0.");
+        }
+    }
+}
