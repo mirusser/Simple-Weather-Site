77e75fabf9b9e8b711e994813474e74ef7eaacfe
mirusser <mirusser@gmail.com>
2024-04-14 10:07:37 +0200

    Refactored publish.sh

--- src/publish.sh
+++ src/publish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# There are so many comments here, 
+# cause I'm not really familiar with bash scripting
+# and I'm now writing it often - mea culpa
+
 # This script is meant to run locally for dev purposes
 # (Run with sudo)
 
@@ -10,132 +14,123 @@ RED='\033[0;31m' # for error messages
 BLUE='\033[0;34m' # for custom informative messages
 NC='\033[0m' # No Color - for default/informative messages
 
-# Network name
-network_name="overlaynetwork"
+# Defines where solution and its docker-compose.yml files are located
+BASE_DIR="${1:-/home/arnie/Repos/Projects/Simple-Weather-Site/src}"
 
-# Check if the network already exists
-if ! docker network ls --format "{{.Name}}" | grep -wq $network_name; then
-    # Create the Docker network since it doesn't exist
-    docker network create -d bridge "$network_name"
-    echo -e "${GREEN}Network $network_name created.${NC}"
-else
-    echo -e "${BLUE}Network $network_name already exists.${NC}"
-fi
+check_dependencies() {
 
-# Define a base directory variable for easier path management
-# Change path to reflect your local path 
-BASE_DIR="/home/arnie/Repos/Projects/Simple-Weather-Site/src"
-PUBLISH_DIR="/home/arnie/Publish/SimpleWeather"
+    # Make sure that iptables,iptables-persistent, netstat packages are installed, if not run:
+    # sudo apt-get update
+    # sudo apt-get install iptables
+    # sudo apt-get install net-tools
 
-# CitiesGrpcService
-rm -rf "$PUBLISH_DIR/CitiesService/CitiesGrpcService/deploy"
-mkdir -p "$PUBLISH_DIR/CitiesService/CitiesGrpcService/deploy"
-cd "$BASE_DIR/CitiesService/CitiesGrpcService" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/CitiesService/CitiesGrpcService/deploy"
-docker build -t citiesgrpcservice .
+    # Make sure that iptables is added to path
+    # echo $PATH
+    # if not then add it (add the end of the file):
+    # nano ~/.bashrc
+    # export PATH=$PATH:/usr/sbin
 
-# CitiesService
-rm -rf "$PUBLISH_DIR/CitiesService/CitiesService.Api/deploy"
-mkdir -p "$PUBLISH_DIR/CitiesService/CitiesService.Api/deploy"
-cd "$BASE_DIR/CitiesService/CitiesService.Api" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/CitiesService/CitiesService.Api/deploy"
-docker build -t citiesservice .
+    local dependencies=("docker" "netstat" "iptables")
+    for dep in "${dependencies[@]}"; do
+        if ! command -v $dep &> /dev/null; then
+            echo "$dep could not be found. Please install $dep."
+            exit 1
+        fi
+    done
+}
 
-# WeatherService
-rm -rf "$PUBLISH_DIR/WeatherService/deploy"
-mkdir -p "$PUBLISH_DIR/WeatherService/deploy"
-cd "$BASE_DIR/WeatherService" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/WeatherService/deploy"
-docker build -t weatherservice .
+create_network(){
 
-# WeatherHistoryService
-rm -rf "$PUBLISH_DIR/WeatherHistoryService/deploy"
-mkdir -p "$PUBLISH_DIR/WeatherHistoryService/deploy"
-cd "$BASE_DIR/WeatherHistoryService" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/WeatherHistoryService/deploy"
-docker build -t weatherhistoryservice .
+    # Network used by docker containers
+    local network_name="overlaynetwork"
 
-# EmailService
-rm -rf "$PUBLISH_DIR/EmailService/EmailService.Api/deploy"
-mkdir -p "$PUBLISH_DIR/EmailService/EmailService.Api/deploy"
-cd "$BASE_DIR/EmailService/EmailService.Api" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/EmailService/EmailService.Api/deploy"
-docker build -t emailservice .
+    # Check if the network already exists
+    if ! docker network ls --format "{{.Name}}" | grep -wq $network_name; then
+        # Create the Docker network since it doesn't exist
+        docker network create -d bridge "$network_name"
+        echo -e "${GREEN}Network $network_name created.${NC}"
+    else
+        echo -e "${BLUE}Network $network_name already exists.${NC}"
+    fi
+}
 
-# IconService
-rm -rf "$PUBLISH_DIR/IconService/IconService.Api/deploy"
-mkdir -p "$PUBLISH_DIR/IconService/IconService.Api/deploy"
-cd "$BASE_DIR/IconService/IconService.Api" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/IconService/IconService.Api/deploy"
-docker build -t iconservice .
+build_and_dockerize() {
 
-# SignalRServer
-rm -rf "$PUBLISH_DIR/SignalRServer/deploy"
-mkdir -p "$PUBLISH_DIR/SignalRServer/deploy"
-cd "$BASE_DIR/SignalRServer" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/SignalRServer/deploy"
-docker build -t signalrserver .
+    local project_local_dir="$1"
+    local docker_image_name="$2"
 
-# HangfireService
-rm -rf "$PUBLISH_DIR/HangfireService/deploy"
-mkdir -p "$PUBLISH_DIR/HangfireService/deploy"
-cd "$BASE_DIR/HangfireService" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/HangfireService/deploy"
-docker build -t hangfireservice .
+    local project_dir="$BASE_DIR/$project_local_dir"
+    local publish_dir="$project_dir/deploy"
 
-# WeatherSite
-rm -rf "$PUBLISH_DIR/WeatherSite/Site/deploy"
-mkdir -p "$PUBLISH_DIR/WeatherSite/Site/deploy"
-cd "$BASE_DIR/WeatherSite/Site" || exit
-dotnet publish -c Release --property:PublishDir="$PUBLISH_DIR/WeatherSite/Site/deploy"
-docker build -t weathersite .
+    # Clean up the previous publish directory and create a new one
+    rm -rf "$publish_dir"
+    mkdir -p "$publish_dir"
 
-# Build and run with docker-compose
-docker compose build
-docker compose -p "sws-containers" up -d
+    # Navigate to the project directory or exit if it fails
+    cd "$project_dir" || { echo -e "${RED}Failed to navigate to $project_dir.${NC}"; exit 1; }
 
-echo -e "${GREEN}'The containers started in the background.${NC}"
+    # Publish the .NET project and build the Docker image
+    dotnet publish -c Release --property:PublishDir="$publish_dir" && \
+    docker build -t "$docker_image_name" . || \
+    { echo -e "${RED}Build or Docker build failed for $docker_image_name.${NC}"; exit 1; }
+}
 
-# Make sure that iptables,iptables-persistent, netstat packages are installed, if not run:
-# sudo apt-get update
-# sudo apt-get install iptables
-# sudo apt-get install net-tools
+build_and_run_docker_compose(){
 
-# Make sure that iptables is added to path
-# echo $PATH
-# if not then add it (add the end of the file):
-# nano ~/.bashrc
-# export PATH=$PATH:/usr/sbin
+   docker compose build && docker compose -p "sws-containers" up -d && \
+    echo -e "${GREEN}=> The containers started in the background.${NC}" || \
+    { echo -e "${RED}=> Docker compose failed.${NC}"; exit 1; }
+}
 
+forward_ports(){
 
-# TODO: maybe move the port listening to another script
-# Define an array of ports you want to check
-ports=(5672 27017 6379 5432 5341 1435 1433 9200 9300)
-
-# Loop through each port in the array
-for PORT in "${ports[@]}"; do
-    echo "Checking port: $PORT"
+    # Define an array of ports to check
+    ports=(5672 27017 6379 5432 5341 1435 1433 9200 9300)
     
-    # Check if the port is already allowed in the iptables firewall
-    if ! iptables -C INPUT -p tcp --dport $PORT -j ACCEPT 2>/dev/null; then
-        echo -e "${YELLOW}Port $PORT is not allowed in iptables. Adding rule to allow it.${NC}"
-        iptables -I INPUT -p tcp --dport $PORT -j ACCEPT
-        # Save the iptables rule (uncomment the line corresponding to your system)
-        iptables-save > /etc/iptables/rules.v4 # Debian/Ubuntu
-        # service iptables save # CentOS/RHEL
-        # systemctl restart iptables # If your system uses systemd and iptables
-    else
-        echo -e "${BLUE}Port $PORT is already allowed in iptables.${NC}"
-    fi
+    # Loop through each port in the array
+    for PORT in "${ports[@]}"; do
+        echo "Checking port: $PORT"
+        
+        # Check if the port is already allowed in the iptables firewall
+        if ! iptables -C INPUT -p tcp --dport $PORT -j ACCEPT 2>/dev/null; then
+            echo -e "${YELLOW}Port $PORT is not allowed in iptables. Adding rule to allow it.${NC}"
+            iptables -I INPUT -p tcp --dport $PORT -j ACCEPT
 
-    # Check if the port is listening
-    if ! netstat -tuln | grep -q ":$PORT"; then
-        echo -e "${RED}Port $PORT is not listening. You might need to start the service that uses this port.${NC}"
-    else
-        echo -e "${GREEN}Port $PORT is listening.${NC}"
-    fi
-done
+            # Save the iptables rule (uncomment the line corresponding to your system)
+            iptables-save > /etc/iptables/rules.v4 # Debian/Ubuntu
+            # service iptables save # CentOS/RHEL
+            # systemctl restart iptables # If your system uses systemd and iptables
+        else
+            echo -e "${BLUE}Port $PORT is already allowed in iptables.${NC}"
+        fi
+    
+        # Check if the port is listening
+        if ! netstat -tuln | grep -q ":$PORT"; then
+            echo -e "${RED}Port $PORT is not listening. You might need to start the service that uses this port.${NC}"
+        else
+            echo -e "${GREEN}Port $PORT is listening.${NC}"
+        fi
+    done
+}
 
-# Wait for user input to continue (mimics the pause command in Windows)
-read -p "Press any key to continue . . . "
+check_dependencies
+
+create_network
+
+build_and_dockerize "/CitiesService/CitiesService.Api" "citiesservice"
+build_and_dockerize "/CitiesService/CitiesGrpcService" "citiesgrpcservice"
+build_and_dockerize "/WeatherService" "weatherservice"
+build_and_dockerize "/WeatherHistoryService" "weatherhistoryservice"
+build_and_dockerize "/EmailService/EmailService.Api" "emailservice"
+build_and_dockerize "/IconService/IconService.Api" "iconservice"
+build_and_dockerize "/SignalRServer" "signalrserver"
+build_and_dockerize "/HangfireService" "hangfireservice"
+build_and_dockerize "/WeatherSite/Site" "weathersite"
+
+build_and_run_docker_compose
+
+forward_ports
+
+# Wait for user input to continue
+read -p "Press enter to continue . . ."
 
