1019b85a59ab29ba1377c72bb2757818b64f4250
mirusser <mirusser@gmail.com>
2025-12-13 19:23:45 +0100

    Fixed WeatherService so it starts booting up properly

--- src/WeatherService/Controllers/WeatherForecastController.cs
+++ src/WeatherService/Controllers/WeatherForecastController.cs
@@ -8,30 +8,23 @@ namespace WeatherService.Controllers;
 
 [ApiController]
 [Route("api/[controller]/[action]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController(IMediator mediator) : ControllerBase
 {
-    private readonly IMediator _mediator;
-
-    public WeatherForecastController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpPost]
     public async Task<IActionResult> GetByCityName(GetByCityNameQuery query)
     {
-        return Ok(await _mediator.Send(query));
+        return Ok(await mediator.Send(query));
     }
 
     [HttpPost]
     public async Task<ActionResult<WeatherForecastDto>> GetCityByNameFromXmlResponse(GetByCityNameFromXmlResponseQuery query)
     {
-        return Ok(await _mediator.Send(query));
+        return Ok(await mediator.Send(query));
     }
 
     [HttpPost]
     public async Task<ActionResult<WeatherForecastDto>> GetByCityId(GetByCityIdQuery query)
     {
-        return Ok(await _mediator.Send(query));
+        return Ok(await mediator.Send(query));
     }
 }
